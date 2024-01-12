0fbe70485f37c8fe190d25f2a62c90e533f30eec
3n3a <46775561+3n3a@users.noreply.github.com>
2024-01-13 00:53:01 +0100

    add more output for rdap calc

--- scripts/calc_rdap_usage.sh
+++ scripts/calc_rdap_usage.sh
@@ -9,5 +9,13 @@ source "$SCRIPT_DIR/lib_iana.sh"
 rdap=$(get_tlds_rdap_count)
 normal=$(get_tlds_registered_count)
 
-echo "Calculation of RDAP Support in TLDs: "
+echo "Calculation of RDAP Support in TLDs"
+echo "==================================="
+
+echo ""
+echo "TLDs supporting RDAP: $rdap"
+echo "TLDs in general: $normal"
+echo ""
+
+echo "Percentage of TLDs which Support RDAP: "
 python3 -c "print(str(round($rdap / $normal * 100, 2)) + ' %')"

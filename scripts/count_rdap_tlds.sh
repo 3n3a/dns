df95d7f4ce66ac1f9df5c4c57361f89956611a1c
3n3a <46775561+3n3a@users.noreply.github.com>
2024-01-12 23:07:29 +0100

    Update count_rdap_tlds.sh

--- scripts/count_rdap_tlds.sh
+++ scripts/count_rdap_tlds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "This many tlds have ldap support: " 
+echo "This many tlds have rdap support: " 
 curl -kfsSL https://data.iana.org/rdap/dns.json | jq ".services.[].[0].[]" | wc -l

8450d412adfb1920e3f3583241e5ffe24566e5c7
3n3a <46775561+3n3a@users.noreply.github.com>
2024-01-30 23:11:05 +0100

    Update create_domains_table.sql

--- sql/create_domains_table.sql
+++ sql/create_domains_table.sql
@@ -4,12 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS public.domains
 (
-    id uuid NOT NULL DEFAULT gen_random_uuid(),
-    value text COLLATE pg_catalog."default" NOT NULL,
-    created timestamp with time zone NOT NULL DEFAULT now(),
-    updated timestamp with time zone NOT NULL DEFAULT now(),
-    CONSTRAINT domains_pkey PRIMARY KEY (id),
-    CONSTRAINT uq_value UNIQUE (value)
+    value text,
 )
 
 TABLESPACE pg_default;
